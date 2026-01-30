1be3fd44c20eb7c0b0c016431b37b919f1630f36
anishajoy06 <anishajoy1201@gmail.com>
2026-01-30 23:19:56 +0530

    solved problem 6

---
+++ Task06/Solution.java
@@ -0,0 +1,27 @@
+package Task06;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();   // number of queries
+
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+            int sum = a;
+
+            for (int j = 0; j < n; j++) {
+                sum += b * (1 << j);  // 2^j using bit shift
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
